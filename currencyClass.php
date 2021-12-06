cdbcdabac3537e3554bc564b0079ff5f6f6b4d23
Дмитрий Каминский <dmytro.kaminskyi@gigatrans.ua>
2021-12-06 11:20:21 +0200

    v.1

---
+++ currencyClass.php
@@ -0,0 +1,45 @@
+<?php
+
+class currencyClass
+{
+    /**
+     * @param mixed $country
+     * @return string|array
+     */
+
+    public static function getCurrency(mixed $country): string|array
+    {
+        $url = "https://bank.gov.ua/NBUStatService/v1/statdirectory/exchange?json";
+        $data = file_get_contents($url);
+        $data = json_decode($data, true);
+        $currency = [];
+
+        if (is_array($country)) {
+            foreach ($country as $item) {
+
+                foreach ($data as $countries) {
+                    if ($countries['cc'] === $item) {
+                        $currency[$item] = [
+                            "rate" => $countries['rate'],
+                            "exchange-date" => $countries['exchangedate']
+                        ];
+                    }
+                }
+            }
+            return $currency;
+
+        }
+
+        foreach ($data as $countries) {
+            if ($countries['cc'] === $country) {
+                $currency[$country] = [
+                    "rate" => $countries['rate'],
+                    "exchange-date" => $countries['exchangedate']
+                ];
+            }
+        }
+
+
+        return $currency;
+    }
+}
